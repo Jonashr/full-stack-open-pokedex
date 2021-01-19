ed4347c3ae280b756fa7180e602d0b9e71872912
jonashr <jonashorvei1@gmail.com>
2021-01-19 17:06:54 +0100

    More linting errors..

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-console.log('PORT', PORT)
-
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
