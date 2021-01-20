adf57b666292ac1f8101b725f94e173f82d55217
jonashr <jonashorvei1@gmail.com>
2021-01-20 14:29:40 +0100

    Added another cypress e2e test.

--- cypress/integration/pokemon_api.js
+++ cypress/integration/pokemon_api.js
@@ -4,4 +4,13 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon slowpoke page can be navigated to and when clicking the next button slowbro is the next pokemon', function() {
+    cy.visit('http://localhost:5000/')
+    cy.contains('slowpoke')
+    cy.contains('slowpoke').click()
+    cy.location().should((loc) => expect(loc.toString()).to.eq('http://localhost:5000/pokemon/slowpoke'))
+    cy.contains('Next').click()
+    cy.contains('slowbro')
+  })
 })
